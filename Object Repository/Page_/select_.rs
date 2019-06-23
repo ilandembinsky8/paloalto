7aef29b6abe173cb0a30dbd89b34ecf6fbf4d4a0
ilan <ilan@yan-systems.co.il>
2019-06-23 07:05:22 +0300

    first commit

---
+++ Object Repository/Page_/select_.rs
@@ -0,0 +1,177 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_</name>
+   <tag></tag>
+   <elementGuidId>45c75c78-48e9-4090-9f83-9aa19d46301e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='סוג העסק'])[2]/following::select[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onchange</name>
+      <type>Main</type>
+      <value>refreshCalcList(this.value)</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                בחר סוג עסק
+                
+                    אולמות וגני אירועים
+                    
+                    אולמות כנסים
+                    
+                    אולמות משחק / משטחי החלקה
+                    
+                    אולמות תיאטרון / קולנוע ותערוכות קבועות
+                    
+                    אולמות תצוגה
+                    
+                    איצטדיונים, היכלי ספורט
+                    
+                    אירוע שמחה חד פעמי באולם או גן אירועים
+                    
+                    אירועים חד פעמיים
+                    
+                    ארועים: בתי כנסת
+                    
+                    ארועים: מועדון בר / בת מצווה
+                    
+                    בנקים / סניפי דואר
+                    
+                    בריכות שחייה
+                    
+                    בתי אבות / דיור מוגן
+                    
+                    בתי מלון
+                    
+                    בתי מלון דמי רשיון זמניים לשנת 2016
+                    
+                    בתי קפה / מזנונים
+                    
+                    גלידריות
+                    
+                    הרקדות
+                    
+                    חברות הסעה ואוטובוסים
+                    
+                    חדרי המתנה / מרפאות / משרדים
+                    
+                    חנויות
+                    
+                    חנויות אופנה
+                    
+                    חנויות סופרמרקט
+                    
+                    חניונים
+                    
+                    לונה פארק
+                    
+                    מוסדות חינוך, הכשרה מקצועית והשכלה גבוהה
+                    
+                    מועדוני כושר / סטודיו חוגים
+                    
+                    מועדונים ודיסקוטקים
+                    
+                    מועצות איזוריות
+                    
+                    מכון יופי וטיפוח / סלון כלות
+                    
+                    מסעדה הפתוחה אחרי 24:00
+                    
+                    מסעדות
+                    
+                    מסעדות חוף
+                    
+                    מספרות
+                    
+                    מקומות עבודה
+                    
+                    מרכזיות טלפונים
+                    
+                    משתלות
+                    
+                    מתנסים / מרכזים קהילתיים
+                    
+                    ספא
+                    
+                    פאבים / בארים
+                    
+                    פארק מים
+                    
+                    פארקים / גנים / ירידים
+                    
+                    קאנטרי קלאב / מועדוני ספורט
+                    
+                    קיבוצים / בתי הארחה / צימרים
+                    
+                    קניונים מרכזי קניות
+                    
+                    רשויות מקומיות
+                    
+                    תחנות דלק
+                    
+                    תערוכות מתחלפות
+                    
+            </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[@class=&quot;INDlangdirRTL INDpositionRight INDDesktop INDChrome&quot;]/div[@class=&quot;theSite&quot;]/div[@class=&quot;outerMargin&quot;]/div[@class=&quot;centerLeft&quot;]/div[1]/div[3]/div[2]/select[1]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='סוג העסק'])[2]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='מחשבון לעסקים'])[2]/following::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='גודל העסק'])[2]/preceding::select[1]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='שעתוק - האם המוסיקה משודרת במדיה המקורית שלה או שהיא הועברה למדיה אחרת, למשל מחשב'])[2]/preceding::select[2]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/div/div[3]/div[2]/select</value>
+   </webElementXpaths>
+</WebElementEntity>
